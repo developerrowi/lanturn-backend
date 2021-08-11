40fc2ade860b83aaf8a98c88cdcffe69ad383440
developerrowi <developerrowi@gmail.com>
2021-08-11 22:04:15 +0800

    Fix Update on getcommon students

--- src/services/GetCommonStudents.js
+++ src/services/GetCommonStudents.js
@@ -9,17 +9,37 @@ class GetCommonStudents {
   async call(query) {
     try {
         let commonStudents = [];
+        let tutorTemp = [];
+        // Check if multiple params
+        if (Array.isArray(query.tutor)) {
 
-        var tutor = await db.Tutor.findAll({ 
-            where: { 
-                email: {
-                    [Op.or]: query.tutor
-                } 
-            }
-        })
-
-        const response = await Promise.all(tutor.map(async (item) => {
-            var tutorStudentTemp = await db.Tutors_Students.findAll({ where: { tutorId: item.id }, include: [db.Student]})
+            var tutor = await db.Tutor.findAll({ 
+                where: { 
+                    email: {
+                        [Op.or]: query.tutor
+                    } 
+                }
+            })
+            const response = await Promise.all(tutor.map(async (item) => {
+                var tutorStudentTemp = await db.Tutors_Students.findAll({ where: { tutorId: item.id }, include: [db.Student]})
+                .then(res=> {
+                    let temp = []
+                    res.map(item => {
+                        temp.push(item.Student.dataValues.email)
+                    })
+                    console.log(temp)
+                    commonStudents.push(temp)
+                })
+            }))
+        } else {
+            // Single Tutor
+            var tutor = await db.Tutor.findOne({ 
+                where: { 
+                    email: query.tutor
+                    
+                }
+            })
+            var tutorStudentTemp = await db.Tutors_Students.findAll({ where: { tutorId: tutor.id }, include: [db.Student]})
             .then(res=> {
                 let temp = []
                 res.map(item => {
@@ -27,7 +47,8 @@ class GetCommonStudents {
                 })
                 commonStudents.push(temp)
             })
-        }))
+        }
+
 
         const data = _.intersection(...commonStudents);
 
